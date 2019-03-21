46c70068f5b68b901e7928f19c37ee94775d2de3
David Dušek <dusek.david5@gmail.com>
2019-03-21 18:17:00 +0100

    initial commit

---
+++ src/main/java/com/cleverlance/ddusek/orikaexamples/OrikaexamplesApplication.java
@@ -0,0 +1,13 @@
+package com.cleverlance.ddusek.orikaexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrikaexamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrikaexamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cleverlance/ddusek/orikaexamples/OrikaexamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cleverlance.ddusek.orikaexamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrikaexamplesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
